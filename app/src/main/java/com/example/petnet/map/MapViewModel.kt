c19f82160f5413992c2e18777ff133f6cc2c4b62
aestepanova <55638984+aestepanova@users.noreply.github.com>
2023-01-16 05:10:23 +0300

    final for defense

--- app/src/main/java/com/example/petnet/map/MapViewModel.kt
+++ app/src/main/java/com/example/petnet/map/MapViewModel.kt
@@ -2,25 +2,13 @@ package com.example.petnet.map
 
 
 
-import android.content.SharedPreferences
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.paging.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
-import model.Content
-import model.ContentResponseDto
-
+import com.yandex.mapkit.mapview.MapView
 import model.Repository
-import java.util.*
 
 
 class MapViewModel(private val repository: Repository) : ViewModel() {
-    var prPage = mutableStateOf(0)
+    private val mapView: MapView? = null
     var loading = false
 
 
--- app/src/main/java/com/example/petnet/ui/MainScreen.kt
+++ app/src/main/java/com/example/petnet/ui/MainScreen.kt
@@ -3,9 +3,12 @@ package com.example.petnet.ui
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Bookmark
@@ -14,12 +17,14 @@ import androidx.compose.material.icons.outlined.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.petnet.auth.AuthenticationViewModel
 import androidx.compose.ui.graphics.Shape.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -27,8 +32,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.petnet.map.MapViewModel
-import com.example.petnet.search.SearchViewModel
+import com.example.petnet.ui.theme.Brown200
 import com.example.petnet.ui.theme.Montserrat
+import com.example.petnet.ui.theme.Satoshi
 import model.Content
 
 
@@ -37,7 +43,6 @@ import model.Content
 @Composable
 fun MainScreen(
     viewModel: MapViewModel,
-    searchViewModel: SearchViewModel,
     hideKeyboard: () -> Unit,
     authViewModel: AuthenticationViewModel
 ) {
@@ -105,13 +110,6 @@ fun MainScreen(
 
         NavHost(navController = navController, startDestination = "Main") {
             composable(route = "Main") { StartScreen(viewModel, navController) }
-            composable(route = "Search") {
-                SearchScreen(
-                    searchViewModel,
-                    navController,
-                    hideKeyBoard = hideKeyboard
-                )
-            }
         }
 
     }
@@ -119,34 +117,40 @@ fun MainScreen(
 
 @Composable
 fun StartScreen(viewModel: MapViewModel, navController: NavController) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
             .clickable { navController.navigate("Feed") }
     ) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentSize(Alignment.Center)) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+        ) {
             Text(
                 "Pet Net", modifier = Modifier.wrapContentSize(Alignment.Center),
                 fontWeight = FontWeight.Bold,
                 fontSize = 42.sp,
-                color = Color(0xFF03A9F4),
-                fontFamily = Montserrat,
+                color = Brown200,
+                fontFamily = Satoshi,
             )
         }
         Spacer(Modifier.height(10.dp))
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentSize(Alignment.Center)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+        ) {
             Text(
                 "Just tap to start",
                 modifier = Modifier.wrapContentSize(Alignment.Center),
                 fontWeight = FontWeight.Medium,
                 fontSize = 20.sp,
                 color = Color.Black,
-                fontFamily = Montserrat,
+                fontFamily = Satoshi,
             )
         }
     }
